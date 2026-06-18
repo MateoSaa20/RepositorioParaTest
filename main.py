cd8399372e4101b14644b70b63ef433429fcb44e
MateoSaa20 <mateosaavedra469@gmail.com>
2026-06-18 18:12:10 -0300

    Se agregaron los archivos para test

---
+++ main.py
@@ -0,0 +1,6 @@
+from calculadora import sumar, restar, multiplicar, dividir
+
+print("Suma:", sumar(10, 5))
+print("Resta:", restar(10, 5))
+print("Multiplicación:", multiplicar(10, 5))
+print("División:", dividir(10, 5))
